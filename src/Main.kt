587efdd6b014517ac82db24027e959a3eec7b58c
MorOlesya <dl9morozova@gmail.com>
2026-01-29 11:14:27 +0300

    Sets in Kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -29,34 +29,55 @@ fun main() {
 //
 //    println(newSolarSystem[8])
 
-    val solarSystem = mutableListOf(
+//val solarSystem = mutableListOf(
+//        "Mercury", "Venus", "Earth", "Mars",
+//        "Jupiter", "Saturn", "Uranus", "Neptune"
+//    )
+//
+//    println(solarSystem.size)
+//
+//    println(solarSystem[2])
+//    println(solarSystem.get(3))
+//
+//    println(solarSystem.indexOf("Earth"))
+//    println(solarSystem.indexOf("Pluto"))
+//
+//    for (planet in solarSystem) {
+//        println(planet)
+//    }
+//
+//    solarSystem.add("Pluto")
+//    solarSystem.add(3, "Sun")
+//
+//    solarSystem[3] = "Future Moon"
+//    println(solarSystem[3])
+//    println(solarSystem[9])
+//
+//    solarSystem.removeAt(9)
+//    solarSystem.remove("Uranus")
+//
+//    println(solarSystem.contains("Pluto"))
+//    println("Uranus" in solarSystem)
+
+    val solarSystem = mutableSetOf(
         "Mercury", "Venus", "Earth", "Mars",
         "Jupiter", "Saturn", "Uranus", "Neptune"
     )
 
     println(solarSystem.size)
 
-    println(solarSystem[2])
-    println(solarSystem.get(3))
-
-    println(solarSystem.indexOf("Earth"))
-    println(solarSystem.indexOf("Pluto"))
-
-    for (planet in solarSystem) {
-        println(planet)
-    }
-
     solarSystem.add("Pluto")
-    solarSystem.add(3, "Sun")
-
-    solarSystem[3] = "Future Moon"
-    println(solarSystem[3])
-    println(solarSystem[9])
-
-    solarSystem.removeAt(9)
-    solarSystem.remove("Uranus")
+    println(solarSystem.size)
 
     println(solarSystem.contains("Pluto"))
-    println("Uranus" in solarSystem)
+    println("Pluto" in solarSystem)
+
+    solarSystem.add("Pluto")
+    println(solarSystem.size)
+
+    solarSystem.remove("Pluto")
+    println(solarSystem.size)
+    println(solarSystem.contains("Pluto"))
+
 }
 
