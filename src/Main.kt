10527cd75e4cca110d1d829f8d883188e41e3c51
MorOlesya <dl9morozova@gmail.com>
2026-01-29 10:44:09 +0300

    Use a singleton object

--- src/Main.kt
+++ src/Main.kt
@@ -1,27 +1,16 @@
 fun main() {
-    val question1 = Question<String>(
-        "Речка спятила с ума – По домам пошла сама. ____",
-        "водопровод",
-        Difficulty.MEDIUM
-    )
+//    println(question1.answer)
+//    println(question2.answer)
+//    println(question3.answer)
+//
+//    println()
+//
+//    println(question1.toString())
+//
+//    println()
 
-    val question2 = Question<Boolean>(
-        "Небо зелёное. Правда или ложь",
-        false,
-        Difficulty.EASY
-    )
+    //println("${StudentProgress.answered} of ${StudentProgress.total} answered")
 
-    val question3 = Question<Int>(
-        "Сколько дней между полнолуниями?",
-        28,
-        Difficulty.HARD
-    )
-
-    println(question1.answer)
-    println(question2.answer)
-    println(question3.answer)
-
-    println()
-
-    println(question1.toString())
+    println("${Quiz.answered} of ${Quiz.total} answered")
 }
+
---
+++ src/StudentProgress.kt
@@ -0,0 +1,26 @@
+
+
+class Quiz{
+    val question1 = Question<String>(
+        "Речка спятила с ума – По домам пошла сама. ____",
+        "водопровод",
+        Difficulty.MEDIUM
+    )
+
+    val question2 = Question<Boolean>(
+        "Небо зелёное. Правда или ложь",
+        false,
+        Difficulty.EASY
+    )
+
+    val question3 = Question<Int>(
+        "Сколько дней между полнолуниями?",
+        28,
+        Difficulty.HARD
+    )
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
